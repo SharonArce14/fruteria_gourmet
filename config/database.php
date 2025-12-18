ffcb081d1197cd44c56e0eab3108538eb09ffbd4
SharonArce14 <arcebeltran2007@gmail.com>
2025-12-18 12:54:11 -0500

    Update database connection user retrieval

--- config/database.php
+++ config/database.php
@@ -2,18 +2,16 @@
 
 function conectarDB() {
     $host = "localhost";
-    $usuario = "fruteria_user";
+    $usuario = getenv('DB_USER') ?: "root";
     $password = getenv('DB_PASSWORD') ?: "";
     $basedatos = "fruteria_gourmet";
     
     $conn = new mysqli($host, $usuario, $password, $basedatos);
     
     if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
+        die("Error de conexion: " . $conn->connect_error);
     }
     
     $conn->set_charset("utf8");
     return $conn;
 }
-
-?>
